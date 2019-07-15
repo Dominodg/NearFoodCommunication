14fef340c68009bf4d571373e53d1f8b7cb8698e
goiad <goia.darius@yahoo.com>
2019-07-15 23:53:41 +0300

    Started working on the nfc function

--- app/src/main/java/com/nearfoodcommunication/main/MainActivity.java
+++ app/src/main/java/com/nearfoodcommunication/main/MainActivity.java
@@ -1,18 +1,47 @@
 package com.nearfoodcommunication.main;
 
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
+
+    public static String TAG = "NFCLINK" + MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "getLink");
+
         setContentView(R.layout.activity_main);
+    }
+
+
+    NfcAdapter mNfcAdapter;
+
+    public void turnOnNfcBeam(){
+
+        if(mNfcAdapter == null)
+        {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+        if(mNfcAdapter == null || !mNfcAdapter.isEnabled())
+        {
+            mNfcAdapter = null;
+            Toast.makeText(this, "NFC is not available. Try the QR code instead.", Toast.LENGTH_LONG);
+        }
+
     }
 
     public void login(View view)
